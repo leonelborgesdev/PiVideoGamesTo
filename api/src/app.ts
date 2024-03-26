08e4378004891e3a7f42309763923732a2cde706
leonelborgesdev <leonel03t@gmail.com>
2024-03-26 10:16:58 -0400

    videogames routes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import videoGameRouter from "./routes/videogame.routes";
 
 const app=express();
 
 app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
+
+app.use(videoGameRouter)
 
 
 export default app;
